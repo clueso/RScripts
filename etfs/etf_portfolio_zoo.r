f75ed62799b00b1872100ec56bf226c095c96a33
clueso <clueso@gmail.com>
2016-03-15 23:59:52 -0700

    Portfolio

--- etfs/etf_portfolio_zoo.r
+++ etfs/etf_portfolio_zoo.r
@@ -7,11 +7,11 @@ download_clip_data = function(sym_list)
 {
 	for (i in 1:length(sym_list)) {
 		if (i == 1) {
-			stock_price = get.hist.quote(sym_list[i], end = "2014-07-01", compression="d",quote="AdjClose",retclass="zoo");
+			stock_price = get.hist.quote(sym_list[i], compression="d",quote="AdjClose",retclass="zoo");
 			max_date = start(stock_price);
 			min_date = end(stock_price);
 		} else {
-			new_data = get.hist.quote(sym_list[i], end = "2014-07-01", compression="d",quote="AdjClose",retclass="zoo")
+			new_data = get.hist.quote(sym_list[i], compression="d",quote="AdjClose",retclass="zoo")
 			if (max_date < start(new_data))
 				max_date = start(new_data);
 			if (min_date > end(new_data))
@@ -32,14 +32,33 @@ calc_returns = function(sym_data)
 
 calc_single_investment_value = function(inv_amount, sym_data)
 {
-	inv_growth = sweep(sym_data, 2, sym_data[1,], "/")
+	inv_growth = sweep(sym_data, 2, sym_data[1,], "/");
 	return(inv_growth * inv_amount);
 }
 
-list = c("XUS.TO", "XCH.TO", "XID.TO");
+calc_portfolio_value = function(inv_amount, sym_data, allocation)
+{
+	ret_val = calc_single_investment_value(inv_amount, sym_data);
+	return(rowSums(sweep(ret_val, MARGIN=2,allocation, '*')));
+}
+
+
+
+#list = c("XUS.TO", "VAB.TO", "VE.TO");
+list = c("SPY", "XIU.TO", "XBB.TO");
+fractions = c(0.33, 0.33, 0.34);
+
+colours = c(1:length(list));
+line_width = c(2,2);
 full_data = download_clip_data(list);
+full_data[as.Date("2000-11-23"), "SPY"] = full_data[as.Date("2000-11-24"), "SPY"];
 full_returns_data = calc_returns(full_data);
 inv_growth_data = calc_single_investment_value(10000, full_data);
-plot(inv_growth_data, plot.type = "single", col = c("red", "blue"), lwd = 2);
+port_growth_data = calc_portfolio_value(10000, full_data, fractions);
+inv_growth_data = merge(inv_growth_data, port_growth_data);
+#col = c("red", "blue", "green", "black", "orange")
+plot(inv_growth_data, plot.type = "single", col = colours, lwd = line_width);
+lines(port_growth_data, col = "slateblue", lwd = line_width);
+#legend(x = as.Date("2013-09-01"), y = 19000, legend = colnames(full_returns_data), col = colours, lwd = line_width);
 
 #par(mfrow = c(2,2));
