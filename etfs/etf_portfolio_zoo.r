63d376045cbb7ef17cf7603a690ee4e9f5230e3f
Ashwin Usgaocar <ausgaocar@fortinet.com>
2016-03-17 14:09:57 -0700

    Portfolio rebalance

--- etfs/etf_portfolio_zoo.r
+++ etfs/etf_portfolio_zoo.r
@@ -41,8 +41,26 @@ calc_portfolio_value = function(inv_amount, sym_data, allocation)
 	ret_val = calc_single_investment_value(inv_amount, sym_data);
 	return(rowSums(sweep(ret_val, MARGIN=2,allocation, '*')));
 }
+calc_shares = function(prices, investments)
+{
+	return(investments/prices);
+}
 
-
+calc_rebalanced_portfolio_value = function(inv_amount, sym_data, rebalance_alloc)
+{
+	sym_coredata = coredata(sym_data);
+	no_of_shares = matrix(nrow = dim(sym_data)[1], ncol = dim(sym_data)[2]);
+	inv_capital = inv_amount * rebalance_alloc;
+	no_of_shares[1,] = calc_shares(sym_data[1,], inv_capital);
+	for (i in 2:dim(sym_data)[1]) {
+		inv_capital = sum(sym_data[i,] * no_of_shares[i-1,]) * rebalance_alloc;
+		no_of_shares[i,] = calc_shares(sym_data[i,], inv_capital);
+	}
+	no_of_shares_zoo = as.zoo(no_of_shares);
+	index(no_of_shares_zoo) = index(sym_data);
+	colnames(no_of_shares_zoo) = colnames(sym_data);
+	return(no_of_shares_zoo);
+}
 
 #list = c("XUS.TO", "VAB.TO", "VE.TO");
 list = c("SPY", "XIU.TO", "XBB.TO");
@@ -56,9 +74,10 @@ full_returns_data = calc_returns(full_data);
 inv_growth_data = calc_single_investment_value(10000, full_data);
 port_growth_data = calc_portfolio_value(10000, full_data, fractions);
 inv_growth_data = merge(inv_growth_data, port_growth_data);
+nos = calc_rebalanced_portfolio_value(10000, full_data, fractions);
 #col = c("red", "blue", "green", "black", "orange")
-plot(inv_growth_data, plot.type = "single", col = colours, lwd = line_width);
-lines(port_growth_data, col = "slateblue", lwd = line_width);
+#plot(inv_growth_data, plot.type = "single", col = colours, lwd = line_width);
+#lines(port_growth_data, col = "slateblue", lwd = line_width);
 #legend(x = as.Date("2013-09-01"), y = 19000, legend = colnames(full_returns_data), col = colours, lwd = line_width);
 
 #par(mfrow = c(2,2));
