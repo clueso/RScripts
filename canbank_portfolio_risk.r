b7ce8ab6e739b64352c8535efa3b183db9fc2376
Ashwin Usgaocar <ausgaocar@fortinet.com>
2016-05-27 11:58:00 -0700

    Added risk scripts

---
+++ canbank_portfolio_risk.r
@@ -0,0 +1,19 @@
+source("etfs/etf_portfolio_functions.r");
+
+
+canbank = c("RY.TO", "TD.TO", "BMO.TO", "CM.TO");
+#list = c("SPY", "XIU.TO", "XBB.TO", "INDY");
+
+colours = c("red", "blue", "green", "black", "darkmagenta");
+fractions = c(0.4, 0.25, 0.1, 0.25);
+
+full_data = download_clip_data(canbank, freq="d");
+full_returns_data = calc_returns(full_data);
+full_data_means = apply(full_returns_data, MARGIN=2, FUN=mean);
+full_data_sds = apply(full_returns_data, MARGIN=2, FUN=sd);
+full_data_cov = cov(full_returns_data);
+full_data_cor = cor(full_returns_data);
+
+#plot_histograms(full_returns_data);
+plot_boxplots(full_returns_data);
+#plot_qqplots(full_returns_data);
