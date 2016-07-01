d603720220f5fd137b04c3951f7fe6e87e51f95c
Ashwin Usgaocar <ausgaocar@fortinet.com>
2016-06-30 17:16:05 -0700

    Correlation between stocks

--- all_symbols.r
+++ all_symbols.r
@@ -2,42 +2,25 @@ source("etfs/etf_portfolio_functions.r");
 
 chosen_list = c("RY.TO", "PLI.TO", "AVO.TO");
 
-#all_data = read.table("./TSX_Stock_list.txt",sep='\t');
-#test_list = all_data[,1];
 test_list = c("TD.TO", "ABT.TO")
-#rm(all_data);
-
-# Method 1 - same effect as method 2
-#test_list = setdiff(test_list, chosen_list);
-#combined_list = c(test_list, chosen_list);
-#full_combined_data = download_clip_data(combined_list);
-#full_test_data = as.zoo(full_combined_data[,1:length(test_list)]);
-#full_chosen_data = as.zoo(full_combined_data[,(length(test_list) + 1):(length(test_list) + length(chosen_list))]);
-
-# Method 2
-#full_chosen_data = calc_returns(download_clip_data(chosen_list));
+full_chosen_data = calc_returns(download_clip_data(chosen_list));
 full_cor_matrix = c();
 for (stock in test_list) {
-	tmp_stock_list = c(chosen_list, stock);
-	full_data = calc_returns(download_clip_data(tmp_stock_list));
-	chosen_data_end = length(chosen_list);
-	full_chosen_data = full_data[,1:chosen_data_end];
-	full_test_data = full_data[,(chosen_data_end+1)];
-	cor_matrix = cor(full_chosen_data, full_test_data);
+	full_test_data = calc_returns(download_clip_data(stock));
+	if (nrow(full_test_data) > nrow(full_chosen_data)) {
+		start = index(full_chosen_data)[1];
+		stop = index(full_chosen_data)[nrow(full_chosen_data)];
+		test_data = window(full_test_data, start = start, end = stop);
+		chosen_data = full_chosen_data;
+	} else {
+		start = index(full_test_data)[1];
+		stop = index(full_test_data)[nrow(full_test_data)];
+		chosen_data = window(full_chosen_data, start = start, end = stop);
+		test_data = full_test_data;
+	}
+	cor_matrix = cor(chosen_data, test_data);
 	full_cor_matrix = cbind(full_cor_matrix, cor_matrix);
 }
 
-#full_test_data = calc_returns(download_clip_data(test_list));
-#if (length(full_test_data) > length(full_chosen_data)) {
-#	start = index(full_chosen_data)[1];
-#	stop = index(full_chosen_data)[nrow(full_chosen_data)];
-#	full_test_data = window(full_test_data, start = start, end = stop);
-#} else {
-#	start = index(full_test_data)[1];
-#	stop = index(full_test_data)[nrow(full_test_data)];
-#	full_chosen_data = window(full_chosen_data, start = start, end = stop);
-#}
-
-#full_cor_matrix = cor(full_chosen_data, full_test_data);
 rownames(full_cor_matrix) = chosen_list;
 colnames(full_cor_matrix) = test_list;
--- portfolio_compare.r
+++ portfolio_compare.r
@@ -1,24 +1,23 @@
 source("etfs/etf_portfolio_functions.r");
-
-
-semicon = c("INTC", "QCOM", "TSM", "ARMH");
-sp = c("SPY");
-
 colours = c("red", "blue", "green", "black", "darkmagenta");
 line_width = c(2,2,2,2,2);
-fractions = c(0.25, 0.25, 0.25, 0.25);
 
-full_semicon_data = download_clip_data(semicon);
-full_sp_data = download_clip_data(sp);
-full_sp_data = clip_data(full_sp_data, start(full_semicon_data), end(full_semicon_data));
+port1 = c("RY.TO", "TD.TO");
+port2 = c("RY.TO", "AVO.TO");
 
-port_growth_data = calc_portfolio_value(10000, full_semicon_data, fractions);
-inv_growth_data = calc_single_stock_value(10000, full_sp_data);
+full_port1_data = download_clip_data(port1);
+full_port2_data = download_clip_data(port2);
+data_start = index(full_port2_data)[1];
+data_end = index(full_port2_data)[length(index(full_port2_data))];
+full_port1_data = clip_data(full_port1_data, start = data_start, end = data_end);
 
+port1_growth_data = calc_portfolio_value(10000, full_port1_data, c(0.5, 0.5));
+port2_growth_data = calc_portfolio_value(10000, full_port2_data, c(0.75, 0.25));
 
-full_growth_data = merge(inv_growth_data, port_growth_data);
+full_growth_data = merge(port1_growth_data, port2_growth_data);
 full_growth_data = na.locf(full_growth_data);
-legend_entries = c("SP500", "Tech portfolio");
+legend_entries = c("high", "low");
+
 #png("test.png", width=800);
 plot(full_growth_data, type="l", plot.type = "single", xlab = "Time",
      ylab = "Value of $10000 investment", col = colours, lwd = line_width);
